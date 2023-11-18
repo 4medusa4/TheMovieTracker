4c4876e8233e4954e0e92bbd4af5dba630522dc2
Kraken <warunamadushanka456@gmail.com>
2023-11-18 20:26:29 +0530

    added missing codes
    Movie Controller

--- src/main/java/com/themovietracker/TheMovieTracker/controller/MovieController.java
+++ src/main/java/com/themovietracker/TheMovieTracker/controller/MovieController.java
@@ -2,11 +2,13 @@ package com.themovietracker.TheMovieTracker.controller;
 
 import com.themovietracker.TheMovieTracker.data.Movie;
 import com.themovietracker.TheMovieTracker.service.MovieService;
+import com.themovietracker.TheMovieTracker.service.TmdbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,9 +17,16 @@ import java.util.List;
 public class MovieController {
     @Autowired
     private MovieService movieService;
+    @Autowired
+    private TmdbService tmdbService;
 
     @GetMapping(path = "/api/v1/movies", produces = MediaType.APPLICATION_JSON_VALUE, name = "getMovies")
     public ResponseEntity<List<Movie>> getMovies() {
         return new ResponseEntity<>(this.movieService.getMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "api/v1/tmdb/movie/{id}", produces = MediaType.APPLICATION_JSON_VALUE, name = "getMovieTmdb")
+    public ResponseEntity<String> getMovie(@PathVariable(name = "id") long id) {
+        return new ResponseEntity<>(this.tmdbService.getMovieDetails(id), HttpStatus.OK);
     }
 }
