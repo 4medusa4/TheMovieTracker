8d5cb366892f13987660828b8f6d8bad1e44836e
Medusa <imeshalakshani007@gmail.com>
2023-11-21 15:57:36 +0530

    payment done

--- src/main/java/com/themovietracker/TheMovieTracker/data/Payment.java
+++ src/main/java/com/themovietracker/TheMovieTracker/data/Payment.java
@@ -1,4 +1,55 @@
 package com.themovietracker.TheMovieTracker.data;
 
+import com.mongodb.internal.connection.Time;
+
+import java.util.Date;
+
 public class Payment {
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public Date getPaymentdate() {
+        return paymentdate;
+    }
+
+    public void setPaymentdate(Date paymentdate) {
+        this.paymentdate = paymentdate;
+    }
+
+    public Time getPaymentTime() {
+        return paymentTime;
+    }
+
+    public void setPaymentTime(Time paymentTime) {
+        this.paymentTime = paymentTime;
+    }
+
+    private Long id;
+    private Long amount;
+    private Date paymentdate;
+    private Time paymentTime;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Long amount, Date paymentdate, Time paymentTime) {
+        this.id = id;
+        this.amount = amount;
+        this.paymentdate = paymentdate;
+        this.paymentTime = paymentTime;
+    }
 }
--- src/main/java/com/themovietracker/TheMovieTracker/repository/PaymentRepository.java
+++ src/main/java/com/themovietracker/TheMovieTracker/repository/PaymentRepository.java
@@ -1,4 +1,7 @@
 package com.themovietracker.TheMovieTracker.repository;
 
-public interface PaymentRepository {
+import com.themovietracker.TheMovieTracker.data.Payment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PaymentRepository extends MongoRepository<Payment, String> {
 }
